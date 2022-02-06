e1d393a8e48295d54fb0e8354c2bb48efa91b2b7
Heekyung Koh <kohhokhj@naver.com>
2022-02-06 18:35:30 +0900

    Time: 276 ms (99.29%), Space: 13.8 MB (36.01%) - LeetHub

---
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        count = max_count = 0
+        for num in nums:
+            if num == 1:
+                # Increment the count of 1's by one.
+                count += 1
+            else:
+                # Find the maximum till now.
+                max_count = max(max_count, count)
+                # Reset count of 1.
+                count = 0
+        return max(max_count, count)
+                    
+                
+        
