214c225f99727b86b02172f54e5422eecc6cdc99
Heekyung Koh <kohhokhj@naver.com>
2022-05-12 15:36:39 +0900

    Time: 200 ms (14.95%), Space: 15.7 MB (61.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,7 +1,10 @@
 class Solution:
     
-    def trap(self, height: List[int]) -> int:
-        
+    def trap_twopointer(self, height: List[int]) -> int:
+        '''
+        Runtime: 186 ms O(n)
+        Memory: 15.7 MB
+        '''
         # 리스트가 0이면 return 0
         if not height:
             return 0
@@ -23,3 +26,28 @@ class Solution:
                 right -= 1
         
         return volume
+    
+    def trap(self, height: List[int]) -> int:
+        
+        stack = []
+        volume = 0
+        
+        for i in range(len(height)):
+            
+            # 변곡점을 만나는 경우 = 스택의 마지막 값과 비교
+            while stack and height[i] > height[stack[-1]]:
+                # 스택에서 꺼낸다
+                top = stack.pop()
+            
+                if not len(stack):
+                    break
+                
+                # 이전과의 차이만큼 물 높이 처리
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+            stack.append(i)
+        return volume
+                
+        
