89f3938754ee1ac846f9f23bae579ba4d6e429e9
Heekyung Koh <kohhokhj@naver.com>
2022-05-12 13:47:49 +0900

    Time: 83 ms (65.48%), Space: 15.2 MB (49.70%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -25,17 +25,25 @@ class Solution:
             if complement in nums[i+1:]:
                 return [i, nums[i+1:].index(complement) + (i+1)]
             
+    def twoSum_usingDict(self, nums: List[int], target: int) -> List[int]:
+        '''
+        Runtime: 136 ms
+        Memory: 15.4 MB
+        '''
+        # 키를 num 값을 index로 딕셔너리 저장
+        nums_map = {}
+        for i, num in enumerate(nums):
+            nums_map[num] = i
+
+        # 타겟에서 첫번째 수를 뺀 결과를 키로 조회
+        for i, num in enumerate(nums):
+            if target - num in nums_map and i != nums_map[target-num]:
+                return i, nums_map[target-num]
+    
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-            '''
-            Runtime: 916 ms
-            Memory: 14.9 MB
-            '''
-            # 키를 num 값을 index로 딕셔너리 저장
-            nums_map = {}
-            for i, num in enumerate(nums):
+        
+        nums_map = {}
+        for i, num in enumerate(nums):
+                if target - num in nums_map:
+                    return nums_map[target - num], i
                 nums_map[num] = i
-           
-            # 타겟에서 첫번째 수를 뺀 결과를 키로 조회
-            for i, num in enumerate(nums):
-                if target - num in nums_map and i != nums_map[target-num]:
-                    return i, nums_map[target-num]
