6f4b9a50f61b662ec5af15b2d827a2df6f455f3f
Heekyung Koh <kohhokhj@naver.com>
2022-05-12 13:12:19 +0900

    Time: 916 ms (33.59%), Space: 14.9 MB (76.45%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,21 @@
 class Solution:
     
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-    
+    def twoSum_bruteforce(self, nums: List[int], target: int) -> List[int]:
+        '''
+        Runtime: 4500 ms
+        Memory: 14.9 MB
+        '''
+        
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-        
-        
+                
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in nums[i+1:]:
+                return [i, nums[i+1:].index(complement) + (i+1)]
+            
