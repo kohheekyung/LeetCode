4f7e67bc801a7dba73999f0b87a42159a2ad4c6d
Heekyung Koh <kohhokhj@naver.com>
2022-05-14 14:35:22 +0900

    Time: 284 ms (85.04%), Space: 16.8 MB (53.18%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.py
+++ 561-array-partition-i/561-array-partition-i.py
@@ -1,12 +1,22 @@
 class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
+    def arrayPairSum_sort(self, nums: List[int]) -> int:
         '''
         오름차순으로 정렬하여 두개씩 짝
+        
+        Runtime 313 ms O(n)
+        Memory 16.6 MB
         '''
         nums.sort()
-        
         pairSum = 0
         for i in range(0, len(nums), 2):
             pairSum += nums[i]
-            
         return pairSum
+    
+    def arrayPairSum(self, nums: List[int]) -> int:
+        '''
+        오름차순으로 정렬하여 두개씩 짝
+        
+        Runtime 313 ms O(n)
+        Memory 16.6 MB
+        '''
+        return sum(sorted(nums)[::2])
