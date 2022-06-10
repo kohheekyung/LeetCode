75b7df07f061994f849a57cfe6add55ef0fa2dce
Heekyung Koh <kohhokhj@naver.com>
2022-06-10 13:17:48 +0900

    Time: 49 ms (57.19%), Space: 13.9 MB (31.27%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    
+
         if list1 and not list2:
             return list1
         elif list2 and not list1:
@@ -15,18 +15,21 @@ class Solution:
         
         head = ListNode(-1)
         prev = head
-        
+
+        # 둘 다 None이 아닐 동안
         while list1 and list2:
-            
+            # list1의 값이 더 작으면 prev의 다음 값에 list1 연결하고 list1 업데이트
             if list1.val <= list2.val:
                 prev.next = list1
                 list1 = list1.next
+            # list2의 값이 더 작으면 prev의 다음 값에 list2 연결하고 list2 업데이트
             else:
                 prev.next = list2
                 list2 = list2.next
+            # prev 이동
             prev = prev.next
         
-        
+        # 남은 연결리스트 붙이기
         if list1 is not None:
             prev.next = list1
         else:
