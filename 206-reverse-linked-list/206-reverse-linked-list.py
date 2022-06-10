ff4c194b089239e649fe502f2773076c7c1d2063
Heekyung Koh <kohhokhj@naver.com>
2022-06-10 14:20:49 +0900

    Time: 66 ms (18.89%), Space: 15.4 MB (93.72%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        Time O(n)
+        Space O(1)
+        '''
+        node, prev = head, None
+        
+        
+        while node:
+            # 현재 노드의 다음 노드 저장
+            next = node.next
+            # 현재 노드의 다음 노드 방향 변경
+            node.next = prev
+            # 포인터들 하나씩 옆으로 이동
+            prev = node
+            node = next
+            
+        return prev
+
+            
+            
+            
+    def reverseList_recursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        Time O(n)
+        Space O(n) n번의 재귀함수 호출
+        '''
+        def reverse(node: ListNode, prev: ListNode = None):
+            # 마지막 노드라면 prev 연결리스트 반환
+            if not node:
+                return prev
+            
+            # 현재 노드의 다음 노드 저장
+            next = node.next
+            # 현재 노드의 다음 노드 방향 변경
+            node.next = prev
+
+            # 포인터들 하나씩 옆으로 이동하여 재귀 호출
+            return reverse(next, node)
+        
+        return reverseList(head)
