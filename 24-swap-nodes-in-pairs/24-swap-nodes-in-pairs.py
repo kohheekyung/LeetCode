88247b71859aeca85a81184b6db5f567f11b59c2
Heekyung Koh <kohhokhj@naver.com>
2022-06-10 17:50:50 +0900

    Time: 45 ms (46.45%), Space: 13.9 MB (62.54%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -4,8 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+    def swapPairs_value(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
         root = head
         while head and head.next:
@@ -19,4 +18,43 @@ class Solution:
             head = head.next.next
             
         return root
+    
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        Time O(n)
+        Space O(1)
+        '''
+        root = prev = ListNode(0)
+        prev.next = head 
         
+        while head and head.next:
+            
+            # b가 head를 가리키고 head가 b 다음 노드를 가리키도록함
+            # b가 앞에 있는 것으로 위치 변경
+            b = head.next
+            head.next = b.next
+            b.next = head
+            
+            # prev가 b를 가리키도록함
+            prev.next = b
+            
+            # 다음번 비교를 위해 이동
+            head = head.next
+            prev = prev.next.next
+            
+            
+        return root.next
+    
+    def swapPairs_recursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        Time O(n)
+        Space O(n)
+        '''
+        if head and head.next:
+            prev = head.next
+            # 스왑된 값 리턴받음
+            head.next = self.swapPairs_recursive(prev.next)
+            prev.next = head
+            return prev
+            
+        return head
