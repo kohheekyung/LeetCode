b88f93be05321a8c940aaed0f34d7cea04700e65
Heekyung Koh <kohhokhj@naver.com>
2022-07-02 19:31:56 +0900

    Time: 37 ms (79.01%), Space: 13.8 MB (96.73%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -4,15 +4,18 @@ class Solution:
         jewels: 보석인 돌의 종류들
         stones 내가 가지고 있는 돌들
         
-        내가 가지고 있는 돌들 중 몇개가 보석인지 알고싶다요.
+        내가 가지고 있는 돌들 중 몇개가 보석인지 알고싶다.
+        Time O(n) : 39 ms
+        Space O(n) : 13.8 MB  보석 길이 만큼
         '''
         result = 0
 
         jewels_dict = collections.defaultdict(int)
         
+        # 돌들 중 보석이면 딕셔너리에 개수추가 
         for stone in stones:    
             jewels_dict[stone] += 1
-            
+        # 보석 개수 합
         for jewel in jewels:
             result += jewels_dict[jewel]
         
