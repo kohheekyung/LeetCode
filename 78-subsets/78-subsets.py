28292bbe24ca10538fc2e428a6ad4e8fa6948efa
Heekyung Koh <kohhokhj@naver.com>
2022-07-12 17:41:22 +0900

    Time: 56 ms (41.46%), Space: 14.2 MB (33.96%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,8 +1,10 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets_heekyung(self, nums: List[int]) -> List[List[int]]:
 
         def dfs(curr_subset, start):
-         
+            '''
+            dfs 인자를 index와 현 subsets으로 잡으나 subsets이 줄어드는 방향
+            '''    
             if curr_subset not in result:
                 result.append(curr_subset)
                 start = 0
@@ -25,9 +27,21 @@ class Solution:
 
         return result
 
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        '''
+        dfs 인자를 index와 현 subsets으로 잡으나 subsets이 늘어나는 방향
+        '''    
+        def dfs(index, path):
+            result.append(path)
+            
+            for i in range(index, len(nums)):
+                dfs(i+1, path+[nums[i]]) # 중복하지 않기 때문에 i+1
+                
+        result = []
+        
+        dfs(0, [])
 
-
-
+        return result
 
 
 
