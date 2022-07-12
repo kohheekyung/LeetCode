547116218ef9a281b29a047d78560fb74e6193d1
Heekyung Koh <kohhokhj@naver.com>
2022-07-12 21:42:56 +0900

    Time: 87 ms (90.42%), Space: 14.9 MB (25.51%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -10,29 +10,39 @@ class Solution:
                 return True
 
             for ticket in curr_tickets:
-                
-                if ticket[0] == curr_from:
+            
+                # 티켓들 중에 찾고있는 현 from이 있으면
+                if ticket[0] == curr_from:    
                     
+                    # 다음 from 확인
                     next_from = ticket[1]
         
+                    # 다음 티켓 후보
                     next_tickets = curr_tickets[:]
                     next_tickets.remove(ticket)
                     
+                    # 다음 시작점의 도착점이 있는지 확인
                     next_exist = dfs(next_from, next_tickets)
+                    
+                    # 존재하면 경로 삽입
                     if next_exist:
                         result.append(ticket[0])
                         return True
+                    # 존재하지 않으면 계속 찾음
                     else:
                         continue
+                        
+            # 못찾으면 False 반환
             return False
             
         
-     
+        # smaller lexical order를 위해 sort하고 시작
         tickets = sorted(tickets, key = lambda x:(x[0], x[1]))
     
         
         result = []
         
+        # JFK로 시작 
         dfs("JFK",tickets)
         
         return result[::-1]
