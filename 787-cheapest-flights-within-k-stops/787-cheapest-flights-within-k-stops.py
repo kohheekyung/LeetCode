af80853b6fa93d15c17850f855740647d359bb90
Heekyung Koh <kohhokhj@naver.com>
2022-07-18 16:23:37 +0900

    Time: 112 ms (94.65%), Space: 15.2 MB (54.33%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -24,50 +24,20 @@
  
                     
 #         return -1
-    
-    
-import heapq
 class Solution:
-    
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        
-        # Build the adjacency matrix
-        adj_matrix = [[0 for _ in range(n)] for _ in range(n)]
+    def findCheapestPrice(self, n, flights, src, dst, K):
+        graph = collections.defaultdict(dict)
         for s, d, w in flights:
-            adj_matrix[s][d] = w
-            
-        # Shortest distances array
-        distances = [float("inf") for _ in range(n)]
-        current_stops = [float("inf") for _ in range(n)]
-        distances[src], current_stops[src] = 0, 0
-        
-        # Data is (cost, stops, node)
-        minHeap = [(0, 0, src)]     
-        
-        while minHeap:
-            
-            cost, stops, node = heapq.heappop(minHeap)
-            
-            # If destination is reached, return the cost to get here
-            if node == dst:
-                return cost
-            
-            # If there are no more steps left, continue 
-            if stops == K + 1:
+            graph[s][d] = w
+        pq = [(0, src, K+1)]
+        vis = [0] * n
+        while pq:
+            w, x, k = heapq.heappop(pq)
+            if x == dst:
+                return w
+            if vis[x] >= k:
                 continue
-             
-            # Examine and relax all neighboring edges if possible 
-            for nei in range(n):
-                if adj_matrix[node][nei] > 0:
-                    dU, dV, wUV = cost, distances[nei], adj_matrix[node][nei]
-                    
-                    # Better cost?
-                    if dU + wUV < dV:
-                        distances[nei] = dU + wUV
-                        heapq.heappush(minHeap, (dU + wUV, stops + 1, nei))
-                        current_stops[nei] = stops
-                    elif stops < current_stops[nei]:
-                        #  Better steps?
-                        heapq.heappush(minHeap, (dU + wUV, stops + 1, nei))
-                        
-        return -1 if distances[dst] == float("inf") else distances[dst]
+            vis[x] = k
+            for y, dw in graph[x].items():
+                heapq.heappush(pq, (w+dw, y, k-1))
+        return -1
